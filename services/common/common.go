f9969fa5caf5f79576c472d3354194203f9121f4
小鱼儿小董子 <2781897595@qq.com>
2023-07-01 08:47:35 +0800

    111

---
+++ services/common/common.go
@@ -0,0 +1,10 @@
+package common
+
+// GetCommonData a method
+func GetCommonData() string {
+	return "Common Data"
+}
+
+func GetData() string {
+	return "Data"
+}
---
+++ services/go.mod
@@ -0,0 +1,3 @@
+module github.com/donglei1234/platform/services
+
+go 1.19
